4818991b106ba3fa9fed3bc9a8da11375befcb75
junichi <junichi@gmail.com>
2026-09-25 00:02:02 +0900

    TOPページを実装

--- src/main/java/com/spring/springbootapplication/controller/HomeController.java
+++ src/main/java/com/spring/springbootapplication/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.spring.springbootapplication.controller;
 
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.springbootapplication.entity.User;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        User loginUser = (User) session.getAttribute("loginUser");
+
+        if (loginUser == null) {
+            return "redirect:/login";
+        }
+
         return "index";
     }
 }
